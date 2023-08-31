131e357f7a01cee1871a9a4762516d215c1ebad4
Stigl <realstigl@gmail.com>
2023-08-31 15:47:37 +0000

    ab

---
+++ setup.sh
@@ -0,0 +1,14 @@
+sudo add-apt-repository ppa:team-gcc-arm-embedded/ppa
+sudo apt-get update 
+sudo apt-get upgrade
+sudo apt-get install gcc-arm-none-eabi
+sudo apt update
+sudo apt installcmake make
+git clone https://github.com/raspberrypi/pico-sdk
+mkdir pico-sdk/build
+cd pico-sdk
+cd build
+cmake ..
+make
+
+
