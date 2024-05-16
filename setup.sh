2f17f6f0add3e8f6d41e6bf05c29fb0ce8ed084f
Stigl <realstigl@gmail.com>
2024-05-16 16:24:57 +0200

    Add path and sumbodules

--- setup.sh
+++ setup.sh
@@ -7,8 +7,11 @@ sudo apt install cmake make
 git clone https://github.com/raspberrypi/pico-sdk
 mkdir pico-sdk/build
 cd pico-sdk
+git submodule update --init --recursive
+export PICO_SDK_PATH="$PWD"
+echo export PICO_SDK_PATH="$PWD" >> ~/.bashrc
+# Optional
 cd build
 cmake ..
 make
-
-
+cd ..
